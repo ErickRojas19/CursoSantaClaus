73d49e2899a62de96d0c3c6740e63912a610222e
ErickRojas19 <47455082+ErickRojas19@users.noreply.github.com>
2020-02-07 17:48:22 -0500

    Add files via upload

---
+++ Clase 1/Clase 1/Curso.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clase_1
+{
+    
+    class Curso
+    {
+        public string nombre;
+        public string horario;
+        public float costo;
+        public string profesor;
+
+        public Curso()
+
+        {
+        }
+        public Curso(string nom, string hor, float cos, string pro)
+        {
+            this.nombre = nom;
+            this.horario = hor;
+            this.costo = cos;
+            this.profesor = pro;
+        }
+
+    }
+}
---
+++ Clase 1/Clase 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Clase_1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenidos al instituto Santa Claus: ");
+            Console.WriteLine("\nCatalogo de cursos navideños: ");
+
+            Curso monos1 = new Curso();
+            monos1.nombre = "Moños navideños nivel basico";
+            monos1.horario = "Lunes y miercoles de 8 a 10 A.M";
+            monos1.costo = 75000F;
+            monos1.profesor = "Pepito Julieto";
+            Console.WriteLine("\n{0}\n{1} Costo: {2:C2}\n{3}", monos1.nombre, monos1.horario, monos1.costo,monos1.profesor);
+
+            Curso monos2 = new Curso();
+            monos2.nombre = "Moños curso avanzado";
+            monos2.horario = "Jueves y viernes de 2 a 4 P.M";
+            monos2.costo = 120000F;
+            monos2.profesor = "Julio pepito";
+            Console.WriteLine("\n{0}\n{1} Costo: {2:C2}\n{3}", monos2.nombre, monos2.horario, monos2.costo, monos2.profesor);
+
+            Curso monos3 = new Curso("Farolitos","De Sabado y Lunes de 1 a 4 P.M",200000F,"Pepito Peréz");
+            Console.WriteLine("\n{0}\n{1} Costo: {2:C2}\n{3}", monos3.nombre, monos3.horario, monos3.costo, monos3.profesor);
+
+            Console.ReadKey();
+        }
+        
+        
+    }
+}
