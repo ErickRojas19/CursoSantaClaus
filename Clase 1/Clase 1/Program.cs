a316ad74512b203132bc17940e229bac39a12c2f
ErickRojas19 <47455082+ErickRojas19@users.noreply.github.com>
2020-02-10 16:52:28 -0500

    Update Program.cs

--- Clase 1/Clase 1/Program.cs
+++ Clase 1/Clase 1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Clase_1
 {
@@ -14,21 +14,24 @@ namespace Clase_1
             monos1.horario = "Lunes y miercoles de 8 a 10 A.M";
             monos1.costo = 75000F;
             monos1.profesor = "Pepito Julieto";
-            Console.WriteLine("\n{0}\n{1} Costo: {2:C2}\n{3}", monos1.nombre, monos1.horario, monos1.costo,monos1.profesor);
+            //Console.WriteLine("\n{0}\n{1} Costo: {2:C2}\n{3}\n", monos1.nombre, monos1.horario, monos1.costo,monos1.profesor);
 
             Curso monos2 = new Curso();
             monos2.nombre = "Moños curso avanzado";
             monos2.horario = "Jueves y viernes de 2 a 4 P.M";
             monos2.costo = 120000F;
             monos2.profesor = "Julio pepito";
-            Console.WriteLine("\n{0}\n{1} Costo: {2:C2}\n{3}", monos2.nombre, monos2.horario, monos2.costo, monos2.profesor);
+            //Console.WriteLine("\n{0}\n{1} Costo: {2:C2}\n{3}", monos2.nombre, monos2.horario, monos2.costo, monos2.profesor);
 
             Curso monos3 = new Curso("Farolitos","De Sabado y Lunes de 1 a 4 P.M",200000F,"Pepito Peréz");
-            Console.WriteLine("\n{0}\n{1} Costo: {2:C2}\n{3}", monos3.nombre, monos3.horario, monos3.costo, monos3.profesor);
+            //Console.WriteLine("\n{0}\n{1} Costo: {2:C2}\n{3}", monos3.nombre, monos3.horario, monos3.costo, monos3.profesor);
+            Console.WriteLine(monos1);
+            Console.WriteLine(monos2);
+            Console.WriteLine(monos3);
 
             Console.ReadKey();
         }
-        
-        
+         
     }
 }
+
